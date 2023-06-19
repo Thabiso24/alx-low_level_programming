e9d4244fedc2113330e93f264a32ecb48fed7f80
Thabiso <tphakane@gmail.com>
2023-06-19 07:13:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - point of programming
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+Puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
