87ea72b63d12aacbc8628dc75d93eba58ea98918
Thabiso <tphakane@gmail.com>
2023-06-21 09:33:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet in lowercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			putchar(b);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
