de3ad7ab27bff4b357051685226db18b396184f7
Thabiso <tphakane@gmail.com>
2023-06-21 14:10:35 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Single digit numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int f;
+
+	f = 0;
+	while
+		(f < 10) {
+			putchar(f + '0');
+			f++;
+		}
+	putchar('\n');
+	return (0);
