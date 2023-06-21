46287a39e604bb2f0729c0f97370f20a9204c9e6
Thabiso <tphakane@gmail.com>
2023-06-21 10:29:10 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabets
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			else if ((b != 'q' && b != 'e') && b <= 'z')
+				putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
