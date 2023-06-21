dad983dbf72291bcd0e6c84d9a7cb6142706af2b
Thabiso <tphakane@gmail.com>
2023-06-21 14:50:44 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Numbers of base
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	int q;
+
+	x = 'a';
+	q = 0;
+	while
+		(q < 10) {
+			putchar(q);
+			q++;
+		}
+	while
+		(x <= 'f') {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
