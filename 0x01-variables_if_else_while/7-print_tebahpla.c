ca0caa49e2b2ea8b1cd840466defa6876d8f2e04
Thabiso <tphakane@gmail.com>
2023-06-21 14:31:26 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Alphabets in reverse
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
