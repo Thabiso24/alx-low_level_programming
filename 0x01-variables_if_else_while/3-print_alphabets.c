a2a3a742e986129f12f2c707f058a69da16d4b56
Thabiso <tphakane@gmail.com>
2023-06-21 10:00:54 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabetic lowercase to uppercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	char f;
+
+	b = 'a';
+	f = 'A';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	while
+		(f <= 'Z') {
+			putchar(f);
+			f++;
+		}
+	putchar('\n');
+	return (0);
