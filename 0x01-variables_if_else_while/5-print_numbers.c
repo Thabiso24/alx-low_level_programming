d77eaf955946f487f530d9a9749d40a85c0520a1
Thabiso <tphakane@gmail.com>
2023-06-21 13:57:53 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Single digit numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int f;
+
+	for (f = 0; f < 10; f++)
+		printf("%i", f);
+	putchar('\n');
+	return (0);
+}
