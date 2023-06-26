4891f744afd2a3fe55c37138285986052fb2284d
Thabiso <tphakane@gmail.com>
2023-06-26 13:50:08 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset to 98 - parameter and updates the value it points to
+ * updates the value to 98
+ * @n: input base
+ * Return: n
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
