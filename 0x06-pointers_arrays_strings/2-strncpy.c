f8d8e16c15798faecba5dd1feaaea4db691d08d0
Thabiso <tphakane@gmail.com>
2023-06-29 11:50:46 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that coppies a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int f;
+
+	f = 0;
+	while (f < n && src[f] != '\0')
+	{
+		dest[f] = src[f];
+		f++;
+	}
+	while (f < n)
+	{
+		dest[f] = '\0';
+		f++;
+	}
+	return (dest);
+}
